416b71cf45941a229eaef51f5d394ecd9e26b739
Daniel Steigerwald <steida@Daniels-MacBook-Pro.local>
2018-11-30 03:07:05 +0100

    Add Head to test things. Yay, Next.js is types.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
+import Head from 'next/head';
 import Counter from '../components/Counter';
 
 export default function Index() {
   return (
-    <div>
-      Welcome to Next.js! <Counter />
-    </div>
+    <>
+      <Head>
+        <title>Este</title>
+        <meta name="viewport" content="width=device-width" />
+      </Head>
+      <div>
+        Welcome to Next.js! <Counter />
+      </div>
+    </>
   );
 }
