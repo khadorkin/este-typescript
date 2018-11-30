811cf5e6ed9fb3747197eebabadde0604b000f26
Daniel Steigerwald <steida@Daniels-MacBook-Pro.local>
2018-11-30 04:04:58 +0100

    Add React Native Web
    Tree shaking just works.
    https://este-plnhtclzvx.now.sh/

--- next.config.js
+++ next.config.js
@@ -1,2 +1,15 @@
+const withTM = require('next-plugin-transpile-modules');
 const withTypescript = require('@zeit/next-typescript');
-module.exports = withTypescript();
+
+module.exports = withTypescript(
+  withTM({
+    transpileModules: ['react-native-web'],
+    webpack: config => {
+      // Alias all `react-native` imports to `react-native-web`
+      config.resolve.alias = {
+        'react-native$': 'react-native-web',
+      };
+      return config;
+    },
+  }),
+);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,29 @@
+import { StyleSheet, Text, View } from 'react-native';
 import Head from 'next/head';
 import Counter from '../components/Counter';
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    alignItems: 'center',
+    fontSize: 24,
+  },
+});
 
 export default function Index() {
   return (
     <>
       <Head>
         <title>Este</title>
-        <meta name="viewport" content="width=device-width" />
       </Head>
-      <div>
-        Welcome to Next.js! <Counter />
-      </div>
+      <View style={styles.container}>
+        <Text style={styles.text}>Welcome to Next.js!</Text>
+        <Counter />
+      </View>
     </>
   );
 }
