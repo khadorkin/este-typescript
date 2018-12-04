9039c11af25b1b5d682afc2fbae67e7f128a2fd7
Daniel Steogerwald <daniel@steigerwald.cz>
2018-12-04 02:23:02 +0100

    import React ftw

--- hooks/useIntl.tsx
+++ hooks/useIntl.tsx
@@ -1,8 +1,8 @@
-import { useContext } from 'react';
+import React from 'react';
 import IntlContext from '../components/IntlContext';
 
 export default function useIntl() {
-  const context = useContext(IntlContext);
+  const context = React.useContext(IntlContext);
   if (context == null) throw Error('useIntl: Please provide IntlContext.');
   return context;
 }
