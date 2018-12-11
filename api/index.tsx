b7f312df97465acb09dbe685f46748544e0f851a
Daniel Steogerwald <daniel@steigerwald.cz>
2018-12-11 18:11:36 +0100

    Shared code in monorepo ftw
    1) esm is must, otherwise import * etc. sucks.
    2) no separate dir, ts-node/register/transpile-only is fast enough

--- api/index.tsx
+++ api/index.tsx
@@ -1,4 +1,10 @@
-// @ts-ignore
-module.exports = (req, res) => {
-  res.end(new Date().toString());
-};
+import * as http from 'http';
+// Just to test code sharing.
+import * as validation from '../validations';
+
+http
+  .createServer((_, res) => {
+    res.write(validation.validateEmail('Hello World!'));
+    res.end();
+  })
+  .listen(3000);
