fcac0f4cf1d6530394e6a190aa729cde00894f11
Daniel Steogerwald <daniel@steigerwald.cz>
2018-12-17 02:32:10 +0100

    Update IntlProviderFix.tsx

--- components/IntlProviderFix.tsx
+++ components/IntlProviderFix.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { injectIntl } from 'react-intl';
 import IntlContext from '../contexts/IntlContext';
 
+// Pass old React context into a new React context so we can have useIntl hook.
+
 interface IIntlProviderFix {
   intl: any;
   children: React.ReactNode;
