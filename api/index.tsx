5c82a794ba9a97ef78cf9d9043e250eb0c165ecb
Daniel Steogerwald <daniel@steigerwald.cz>
2018-12-20 01:11:18 +0100

    Throw when process.env.PRISMA_ENDPOINT is missing

--- api/index.tsx
+++ api/index.tsx
@@ -2,10 +2,15 @@ import { GraphQLServer } from 'graphql-yoga';
 import { Prisma } from '../prisma/generated/prisma-client';
 import { resolvers } from './resolvers';
 
-const db = new Prisma({
-  endpoint: process.env.PRISMA_ENDPOINT as string,
-  secret: process.env.PRISMA_SECRET,
-});
+const endpoint = process.env.PRISMA_ENDPOINT as string;
+if (!endpoint)
+  throw Error(
+    `Missing process.env.PRISMA_ENDPOINT. Did you run 'npm run env dev'?`,
+  );
+
+const secret = process.env.PRISMA_SECRET;
+
+const db = new Prisma({ endpoint, secret });
 
 const server = new GraphQLServer({
   context: { db },
