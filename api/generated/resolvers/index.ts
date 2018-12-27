6667361b270d040313da79c59d706cbebd145520
Daniel Steogerwald <daniel@steigerwald.cz>
2018-12-27 23:20:34 +0100

    Put generated tmp-resolvers
    Because it's good to see updates in diff.

---
+++ api/generated/resolvers/index.ts
@@ -0,0 +1,18 @@
+// This resolver file was scaffolded by github.com/prisma/graphqlgen, DO NOT EDIT.
+// Please do not import this file directly but copy & paste to your application code.
+
+import { Resolvers } from '../graphqlgen';
+
+import { Query } from './Query';
+import { User } from './User';
+import { Mutation } from './Mutation';
+import { SignInPayload } from './SignInPayload';
+import { SignInErrors } from './SignInErrors';
+
+export const resolvers: Resolvers = {
+  Query,
+  User,
+  Mutation,
+  SignInPayload,
+  SignInErrors,
+};
