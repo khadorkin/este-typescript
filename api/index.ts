12f579e1b5736b4e8b40b9c86ffcb8fe9f9bf5e1
Daniel Steogerwald <daniel@steigerwald.cz>
2018-12-29 00:37:32 +0100

    Replace graphql-yoga with Apollo server, add Relay
    https://github.com/prisma/graphql-yoga/issues/449

--- api/index.ts
+++ api/index.ts
@@ -1,26 +1,50 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { ApolloServer, gql } from 'apollo-server';
+import { importSchema } from 'graphql-import';
 import { Prisma } from '../prisma/generated/prisma-client';
 import { resolvers } from './resolvers';
 import { Context } from './types';
+import { IncomingMessage } from 'http';
+import * as jwt from 'jsonwebtoken';
+import { JsonWebTokenPayload } from './resolvers/Mutation';
+// import { makeExecutableSchema } from "graphql-tools";
+// import { rule, shield, and, or, not } from 'graphql-shield';
 
-const endpoint = process.env.PRISMA_ENDPOINT as string;
-if (!endpoint)
-  throw Error(
-    `Missing process.env.PRISMA_ENDPOINT. Did you run 'npm run env dev'?`,
-  );
+const { PRISMA_ENDPOINT, PRISMA_SECRET, API_SECRET } = process.env;
+if (!PRISMA_ENDPOINT || !PRISMA_SECRET || !API_SECRET)
+  throw Error(`Did you run 'npm run env dev'?`);
 
-const context: Context = {
-  db: new Prisma({ endpoint, secret: process.env.PRISMA_SECRET }),
-  hasError: errors => Object.values(errors).some(error => error != null),
+const db = new Prisma({ endpoint: PRISMA_ENDPOINT, secret: PRISMA_SECRET });
+
+const hasError = (errors: {}) =>
+  Object.values(errors).some(error => error != null);
+
+const tryGetUserId = (req: IncomingMessage): string | null => {
+  const { authorization } = req.headers;
+  if (authorization == null) return null;
+  const token = authorization.replace('Bearer ', '');
+  const decoded = jwt.verify(token, API_SECRET);
+  const hasUserId = (decoded: any): decoded is JsonWebTokenPayload =>
+    'userId' in decoded;
+  return hasUserId(decoded) ? decoded.userId : null;
 };
 
-const server = new GraphQLServer({
-  context,
-  resolvers,
-  typeDefs: './api/schema.graphql',
-} as any);
+const createContext = ({ req }: { req: IncomingMessage }): Context => {
+  return {
+    db,
+    hasError,
+    userId: tryGetUserId(req),
+  };
+};
 
-server.start(() => {
+const server = new ApolloServer({
+  context: createContext,
+  resolvers: resolvers as any,
+  typeDefs: gql`
+    ${importSchema(__dirname + '/schema.graphql')}
+  `,
+});
+
+server.listen().then(({ url }) => {
   // tslint:disable-next-line:no-console
-  console.log('Server is running on http://localhost:4000');
+  console.log(`Server ready at ${url}`);
 });
