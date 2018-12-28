0b01eef99036fd3712b5540dae6503f3fe24f106
Daniel Steogerwald <daniel@steigerwald.cz>
2018-12-29 00:39:41 +0100

    Add createRelayEnvironment to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,34 @@ import App, { Container, NextAppContext } from 'next/app';
 import React from 'react';
 import { IntlProvider } from 'react-intl';
 import IntlProviderFix from '../components/IntlProviderFix';
+// import fetch from 'isomorphic-unfetch';
+// import {
+//   Environment,
+//   Network,
+//   RecordSource,
+//   Store,
+//   FetchFunction,
+// } from 'relay-runtime';
+
+// const createRelayEnvironment = (token: string | null, records = {}) => {
+//   const fetchQuery: FetchFunction = async (operation, variables) => {
+//     // TODO: process.env.API_ENDPOINT
+//     const response = await fetch('/graphql', {
+//       body: JSON.stringify({ query: operation.text, variables }),
+//       // lepe? rovnou? hmm, ne
+//       headers: {
+//         'Content-Type': 'application/json',
+//         ...(token != null ? { authorization: `Bearer ${token}` } : null),
+//       },
+//       method: 'POST',
+//     });
+//     return response.json();
+//   };
+//   return new Environment({
+//     network: Network.create(fetchQuery),
+//     store: new Store(new RecordSource(records)),
+//   });
+// };
 
 export default class MyApp extends App<{
   initialNow: number;
